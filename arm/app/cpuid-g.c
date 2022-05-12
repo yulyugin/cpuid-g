c8028a78891116b3eb7cb0b6906304a79c15a2f5
Evgneii Iuliugin <yulyugin@gmail.com>
2022-05-12 12:49:07 +0000

    Restore correct print behavior

--- arm/app/cpuid-g.c
+++ arm/app/cpuid-g.c
@@ -45,7 +45,7 @@
     printf("%-40s %#18x\n", name_string, c->name);
 
 #define PRINT_CPUID_REG64(name_string, name) \
-    printf("%-40s %18" PRIu64 "\n", name_string, c->name);
+    printf("%-40s %#18" PRIx64 "\n", name_string, c->name);
 
 static int
 get_cpuid(void *id, size_t size)
